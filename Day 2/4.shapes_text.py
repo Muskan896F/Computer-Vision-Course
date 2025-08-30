d5c37eb5c81c147dbf6ec31cf547367e5834a520
Muskan Ara <muskan17ara@gmail.com>
2025-08-30 11:49:37 +0530

    day2

--- Day 2/4.shapes_text.py
+++ Day 2/4.shapes_text.py
@@ -6,8 +6,20 @@ print(img.shape)
 img[:] = 255,0,0
 
 # Create a line
-cv2.line(img, (120,55), (300,300), (0,255,0), 3)
+#cv2.line(img, (120,55), (300,300), (0,255,0), 3)
 
+
+
+## Create a rectangle
+#cv2.rectangle(img, (0,0), (250,350), (0,0,255), 7)
+
+
+## Create a circle
+#cv2.circle(img, (400,50), 50, (0,0,255), 5)
+
+
+### Put texts
+cv2.putText(img, "OPENCV", (200,440), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 1)
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
